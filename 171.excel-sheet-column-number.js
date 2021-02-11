e0addc50267abb1f8b88eb30fa5cc106e8ac15f7
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2021-02-11 14:55:14 +0800

    Update #171 solution

--- 171.excel-sheet-column-number.js
+++ 171.excel-sheet-column-number.js
@@ -11,11 +11,18 @@
  */
 var titleToNumber = function(s) {
   const alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  function getIndex(x) {
-    return alphabets.indexOf(x);
+  function getAlphabetPosition(x) {
+    return alphabets.indexOf(x) + 1;
   }
-  const arr = [...s];
-
+  const arr = [...s].reverse();
+  const result = arr.reduce((acc, item, i) => {
+    // console.log({acc, item, i});
+    const pos = getAlphabetPosition(item);
+    const num = (i === 0) ? pos : acc + (Math.pow(26, i) * pos);
+    return num;
+  }, 0);
+  // console.log(result);
+  return result;
 };
 // @lc code=end
 
