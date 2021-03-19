4431c66c3ed0e163192cecb89e7f41f4c0d82e40
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2021-03-19 18:06:57 +0800

    Solve #167

---
+++ 167.two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=167 lang=javascript
+ *
+ * [167] Two Sum II - Input array is sorted
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) return [i + 1, j + 1];
+    }
+  }  
+};
+// @lc code=end
+
