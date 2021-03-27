b84536aaca46335565fa1ca07b2286c03351b247
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2021-03-27 23:12:56 +0800

    Solve #203

---
+++ 203.remove-linked-list-elements.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=203 lang=javascript
+ *
+ * [203] Remove Linked List Elements
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+  if (!head) return head;
+  if (head.val === val) {
+    head = removeElements(head.next, val);
+  } else {
+    head.next = removeElements(head.next, val);
+  }
+  return head;
+};
+// @lc code=end
+
