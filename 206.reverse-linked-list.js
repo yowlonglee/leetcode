5bed4847f0e11e8bf9e97ef3c3dc46649ab8f997
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2021-03-28 22:47:08 +0800

    Solve #206

---
+++ 206.reverse-linked-list.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=206 lang=javascript
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  let prev = null;
+  while (head) {
+    const temp = head.next;
+    head.next = prev;
+    prev = head;
+    head = temp;
+  }
+  return prev;
+};
+// @lc code=end
+
