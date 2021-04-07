e88d64da607bc303d200c72c4fbd737a3676000f
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2021-04-07 13:56:40 +0800

    Add commits

--- 110.balanced-binary-tree.js
+++ 110.balanced-binary-tree.js
@@ -18,18 +18,32 @@
  * @return {boolean}
  */
 var isBalanced = function(root) {
+  // An empty tree is balanced
   if (root === []) return true;
+  // If we get false value on the root, means at least one of the subtree nodes is unbalanced.
+  // We can say this tree is unbalanced.
+  // Else we will get a true value of max tree depth. Then we can return true to say this is a balanced tree.
   return balanceChecker(root) === false ? false : true;
 };
 
+// A helper function to check subtrees in every node
+// Returns false if its subrees are unbalanced
+// Else returns its maxium subtree depth
 function balanceChecker(node) {
+  // Start counting tree depth from leaf
   if (node === null) return 0;
 
+  // Check depth of left and right subtrees
   let left = balanceChecker(node.left);
   let right = balanceChecker(node.right);
   // let val = node.val;
   // console.log({val, left, right});
+
+  // If any subtree returns false value, pass the value to the root
   if (left === false || right === false) return false;
+  // Else we calculate the depth from both trees.
+  // If the difference is greater than 1, means this is an unbalanced tree node.
+  // Else we will pass the max subtree depth to the parent.
   return Math.abs(left - right) > 1 ? false : Math.max(left, right) + 1;
 }
 
