ed696b35e7e710e3cdfa587a434a94b7c63e914f
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2021-04-11 15:55:00 +0800

    Solve #231

---
+++ 231.power-of-two.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=231 lang=javascript
+ *
+ * [231] Power of Two
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+  return (Math.log2(n) % 1 === 0);
+};
+// @lc code=end
+
